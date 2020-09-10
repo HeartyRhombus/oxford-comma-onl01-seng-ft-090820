c3c0abeb96484c98be11abb6a03fae47a21d8925
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 03:06:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   elsif array.length >= 3
-    array.to_sentence
+    array[0..1].join(", ") + "and " + array.last
   end
 end
