afc4671c0ce4ff66b09ffd7a091057058e65e9da
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 02:57:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.to_s
-  elsif array.length == 2
+  if array.length < 3
     array.join(" and ")
+  elsif array.length >= 3
+    array.join(", ")
   end
 end
