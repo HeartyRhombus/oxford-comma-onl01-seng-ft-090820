a267542a651e3c563ab5491330dc9822da0005d8
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 03:18:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
-  elsif array.length >= 3
-    x = array.length - 1
-    array[0..x].join(", ") + ", and " + array.last
+  else
+    new_array = array
+    new_array[-1] = ", and #{new_array[-1]}"
+    new_array.join(", ")
   end
 end
