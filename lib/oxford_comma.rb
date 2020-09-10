6d04767382ad1a43f5972526bdab8cfd82b4d75a
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 03:18:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join(" and ")
   else
     new_array = array
-    new_array[-1] = ", and #{new_array[-1]}"
+    new_array[-1] = " and #{new_array[-1]}"
     new_array.join(", ")
   end
 end
